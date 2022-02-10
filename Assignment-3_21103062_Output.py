bff689492ce8153e2aa52dfa28c5e20e52df04cd
rajatkumarcse <96794010+rajatkumarcse@users.noreply.github.com>
2022-02-10 14:05:51 +0530

    Create Assignment-3_21103062_Output.py

---
+++ Assignment-3_21103062_Output.py
@@ -0,0 +1,106 @@
+# Output ---> (Question --> 1)
+Enter a string :
+Rajat
+{'R': 1, 'a': 2, 'j': 1, 't': 1}
+
+
+# Output ---> (Question --> 2)
+# We will consider two cases
+
+# Case --> 1
+Enter the year between 1800 to 2025 : 1999
+Enter the month between 1 to 12 : 02
+Enter the day between 1 to 31 : 28
+The next date is : 1-3-1999
+
+# Case --> 2
+Enter the year between 1800 to 2025 : 2012
+Enter the month between 1 to 12 : 02
+Enter the day between 1 to 31 : 28
+The next date is : 29-2-2012
+
+
+# Output ---> (Question --> 3)
+Enter the elements separated by a space : 3 9 10
+
+The resultant tuple formed is :
+[(3, 9), (9, 81), (10, 100)]
+
+
+# Output ---> (Question --> 4)
+# We will consider three cases
+
+# Case --> 1
+Enter your grade points : 9
+Your grade is 'A' and 'Excellent' performance
+
+# Case --> 2
+Enter your grade points : 12
+Invalid input
+
+# Case --> 3
+Enter your grade points : 3
+Failed !
+
+
+# Output ---> (Question --> 5)
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+# Output ---> (Question --> 6)
+Enter student's SID : 21103062
+Enter student's name : Rajat
+Do you want to enter another student's details (Y or N) : Y
+Enter student's SID : 21103060
+Enter student's name : Kartik
+Do you want to enter another student's details (Y or N) : Y
+Enter student's SID : 21103035
+Enter student's name : Sumit
+Do you want to enter another student's details (Y or N) : N
+
+# Part (a)
+Students' details = {21103062: 'Rajat', 21103060: 'Kartik', 21103035: 'Sumit'}
+
+# Part (b)
+Sorted list using names = {21103060: 'Kartik', 21103062: 'Rajat', 21103035: 'Sumit'}
+
+# Part (c)
+Sorted list using SID = {21103035: 'Sumit', 21103060: 'Kartik', 21103062: 'Rajat'}
+
+# Part (d)
+Please search student's name with his/her SID : 21103062
+Searched student's name = Rajat
+
+
+# Output ---> (Question --> 7)
+Enter the number of terms in the sequence : 5
+The Fibonacci sequence is :
+0
+1
+1
+2
+3
+The average of the resultant fibonacci sequence is : 1.4
+
+
+# Output ---> (Question --> 8)
+
+# Part (a)
+{1, 3, 5, 6, 8}
+
+# Part (b)
+{3, 6, 8, 9, 13, 17}
+
+# Part (c)
+{1, 2, 4, 5}
+
+# Part (d)
+{6, 7, 8, 9, 10}
+
+# Part (e)
+{10, 7}
